63d120d8ff6fc7f74a94528ee70a9323c2e0bab1
sarahannnicholson <sarah.ann.nicholson@gmail.com>
2016-06-03 12:03:25 -0700

    #10

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -7,11 +7,66 @@
  * # MainCtrl
  * Controller of the artswebclientMasterApp
  */
-angular.module('artswebclientMasterApp')
-  .controller('MainCtrl', function ($scope) {
+var artsWebApp = angular.module('artswebclientMasterApp');
+
+artsWebApp.controller('MainCtrl', function ($scope) {
     $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
+        'HTML5 Boilerplate',
+        'AngularJS',
+        'Karma'
     ];
-  });
+    $scope.createState	= true;
+    $scope.contextState	= false;
+    $scope.contentState	= false;
+    $scope.adjustState	= false;
+    $scope.confirmState	= false;
+
+
+    $scope.switchState = function(){
+    	if($scope.createState){
+ 			
+ 			$scope.createState	= false;
+ 			$scope.contextState	= true;
+
+    	} else if($scope.contextState){
+    		
+ 			$scope.contextState	= false;
+    		$scope.contentState	= true;
+    	
+    	} else if($scope.contentState){
+ 			
+ 			$scope.contentState	= false;
+			$scope.adjustState	= true;
+    	
+    	} else if($scope.adjustState){
+    		
+    		$scope.adjustState	= false;
+ 			$scope.confirmState	= true;
+    	
+    	} else if($scope.confirmState){
+    		
+    		$scope.confirmState	= false;
+ 			$scope.createState	= true;
+    	}
+    };
+});
+
+artsWebApp.controller('createState', function ($scope) {
+    $scope.test = 'test string in createState';
+});
+
+artsWebApp.controller('contextState', function ($scope) {
+    $scope.test = 'test string in contextState';
+});
+
+artsWebApp.controller('contentState', function ($scope) {
+    $scope.test = 'test string in contentState';
+});
+
+artsWebApp.controller('adjustState', function ($scope) {
+    $scope.test = 'test string in adjustState';
+});
+
+artsWebApp.controller('confirmState', function ($scope) {
+    $scope.test = 'test string in confirmState';
+});
